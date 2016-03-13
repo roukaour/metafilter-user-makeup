1a5618a555827c30b3e9e75f094645e670212855
Remy Oukaour <remy.oukaour@gmail.com>
2016-03-13 09:05:40 -0400

    Use formula from W3C accessibility guidelines to calculate contrasting color

--- script.user.js
+++ script.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/roukaour/
 // @description Assigns each MetaFilter user a random but consistent color and symbol.
 // @include     *://*.metafilter.com/*
-// @version     1.3
+// @version     1.4
 // @grant       none
 // @run-at      document-end
 // ==/UserScript==
@@ -21,14 +21,28 @@ function hashCode(s) {
 	return hash;
 } 
 
+function luma(r, g, b) {
+	// sRGB to RGB and RGB to luma formulas from W3C accessibility guidelines
+	// https://www.w3.org/TR/WCAG20/#relativeluminancedef
+	var c = [r, g, b];
+	for (var i = 0; i < 3; i++) {
+		c[i] /= 255;
+		if (c[i] <= 0.03928) {
+			c /= 12.92;
+		} else {
+			c = Math.pow((c + 0.055) / 1.055, 2.4);
+		}
+	}
+	return 0.2126 * c[0] + 0.7152 * c[1] + 0.0722 * c[2];
+}
+
 function hashToColorSpan(x) {
 	// Based on Cristian Sanchez's code:
 	// http://stackoverflow.com/a/3426956/70175
 	var color = (x & 0xFFFFFF).toString(16).toUpperCase();
 	color = '00000'.substring(0, 6 - color.length) + color;
 	var r = (x & 0xFF0000) >> 16, g = (x & 0xFF00) >> 8, b = x & 0xFF;
-	var luma = 0.2126 * r + 0.7152 * g + 0.0722 * b;
-	var contrast = luma >= 165 ? '000' : 'FFF';
+	var contrast = luma(r, g, b) > 0.179 ? '000' : 'FFF';
 	return '<span style="background: #' + color + '; color: #' + contrast + ';">';
 }
 
@@ -47,10 +61,8 @@ function assignColors() {
 		var userlink = byline.getElementsByTagName('a')[0];
 		var username = userlink.innerHTML;
 		var hash = hashCode(username);
-		var colorSpan = hashToColorSpan(hash);
-		var symbol = hashToSymbol(hash);
-		userlink.innerHTML = colorSpan + '<big>' + symbol + '</big> ' +
-			username + '</span>';
+		userlink.innerHTML = hashToColorSpan(hash) +
+			'<big>' + hashToSymbol(hash) + '</big> ' + username + '</span>';
 	}
 }
 
